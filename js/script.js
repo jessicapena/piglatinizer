cf4587cdf44319872439ca651175d3bc50e569f0
jessicapena <jessica@scripted.org>
2019-11-01 18:27:36 +0000

    completed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-$( "document" ).ready(function() {
-
+$( "document" ).ready(function(){
 $("button").click(function(){
     let userInput = $("input").val();
-    let slicer = userInput.slice(1);
-    let pigLatin = userInput.charAt(0);
-    $(".output").text(`${slicer}${pigLatin}ay`);
+    let splitter = userInput.split(" ");
+    console.log(splitter);
+    splitter.forEach(function(element){
+        let pigLatin = element.charAt(0);
+        let slicer = element.slice(1);
+            if (pigLatin === "a"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }else if (pigLatin === "e"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }else if (pigLatin === "i"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }else if (pigLatin === "o"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }else if (pigLatin === "u"){
+                $(".output").append(` ${pigLatin}${slicer}ay`);
+            }
+            else{
+                $(".output").append(` ${slicer}${pigLatin}ay`);
+            }
+    });
 });
-
-
 });
